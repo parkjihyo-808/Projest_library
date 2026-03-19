efeddbf2014f28948b65ec703f44fc391044323a
parkjihyo <wlgy5678@gmail.com>
2026-03-19 17:49:55 +0900

    내일 신청폼 만들기

--- src/main/java/com/busanit501/project_library/service/EventService.java
+++ src/main/java/com/busanit501/project_library/service/EventService.java
@@ -4,6 +4,8 @@ import com.busanit501.project_library.dto.EventDTO;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
+import java.util.List;
+
 public interface EventService {
     Long register(EventDTO eventDTO); // 등록
     EventDTO readOne(Long id);       // 상세 조회
@@ -13,4 +15,11 @@ public interface EventService {
 
     // 2. 카테고리별 목록 페이징 (컨트롤러의 lectureList 메서드용)
     Page<EventDTO> getLecturesByCategory(String category, Pageable pageable);
+
+    // keyword 파라미터 추가
+    Page<EventDTO> getLecturesWithSearch(String keyword, String category, Pageable pageable);
+
+    List<EventDTO> getAllEvents();
+
+    Page<EventDTO> getCinemaWithSearch(String keyword, Pageable pageable);
 }
